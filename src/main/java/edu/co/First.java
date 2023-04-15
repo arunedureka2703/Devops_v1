6112f157e8c64a050d8bfc69e059c873c16a9f92
arunedureka2703 <115924616+arunedureka2703@users.noreply.github.com>
2023-04-15 23:45:05 +0530

    Update First.java

--- src/main/java/edu/co/First.java
+++ src/main/java/edu/co/First.java
@@ -7,8 +7,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class First {
 
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "F:\\Selenium\\chromedriver_win32\\chromedriver.exe");  
-		WebDriver driver = new ChromeDriver();  
+		System.setProperty("webdriver.chrome.driver", "F:\\Selenium\\chromedriver.exe");  
+		//WebDriver driver = new ChromeDriver();  
+		ChromeOptions chromeOptions = new ChromeOptions();
+		 chromeOptions.addArguments("--remote-allow-origins=*");
+		 ChromeDriver driver = new ChromeDriver(chromeOptions); 
 	      
 	// Launch website  
 	    driver.navigate().to("http://www.edureka.co/");  
